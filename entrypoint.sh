6dc4fc5a8fff4c481024848bcb391a967a58b6dc
Roberto Esparza <robertoesparza3@gmail.com>
2020-06-04 18:04:20 -0400

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-ls -la
 export AWS_ACCESS_KEY_ID="$INPUT_AWS_ACCESS_KEY_ID"
 export AWS_SECRET_ACCESS_KEY="$INPUT_AWS_SECRET_ACCESS_KEY"
+
+echo "cuurrent workspace"
+
+ls -la
 
 echo "aws version"
 
