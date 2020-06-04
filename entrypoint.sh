5fd101de7a2a9e2f2465c9ce1b9338a77823e462
Idalio Belmar <idalio.belmar@gmail.com>
2020-06-04 16:46:31 -0400

    init commit

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+ls -la
+export AWS_ACCESS_KEY_ID="$INPUT_AWS_ACCESS_KEY_ID"
+export AWS_SECRET_ACCESS_KEY="$INPUT_AWS_SECRET_ACCESS_KEY"
+
+echo "aws version"
+
+aws --version
+
+echo "Attempting to update kubeconfig for aws"
+
+aws eks --region "$INPUT_AWS_REGION" update-kubeconfig --name "$INPUT_CLUSTER_NAME"
+kubectl "$@"
